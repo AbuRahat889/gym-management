304f2f7b44e288454027d36e4866f40af680f326
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-30 21:18:05 +0600

    gym-management project setup and create navbar

---
+++ src/app/Components/navbar/Navbar.jsx
@@ -0,0 +1,118 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
+
+  const links = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Meals",
+      path: "/meals",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blogs",
+      path: "/blogs",
+    },
+    {
+      title: "Catagorie",
+      path: "/catagories",
+    },
+  ];
+  return (
+    <div>
+      <nav className="relative bg-white shadow dark:bg-gray-800">
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <a href="#">
+              <Image
+                className="w-auto h-6 sm:h-7"
+                src="https://merakiui.com/images/full-logo.svg"
+                alt="Logo"
+                width={100}
+                height={40}
+              />
+            </a>
+
+            {/* Mobile menu button */}
+            <div className="flex lg:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+              >
+                {!isOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 8h16M4 16h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+
+          {/* Mobile Menu */}
+          <div
+            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center ${
+              isOpen
+                ? "translate-x-0 opacity-100"
+                : "opacity-0 -translate-x-full"
+            }`}
+          >
+            <div className="container flex items-center justify-center  mx-auto text-gray-600 capitalize dark:text-gray-300 ">
+              {links?.map((link) => (
+                <Link
+                  className={`${
+                    pathName === link.path && "text-red-500"
+                  } mr-5 text-xl font-bold `}
+                  key={link.path}
+                  href={link.path}
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
